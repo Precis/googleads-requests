23a397c1de117358239ea713d78f8e393772aded
Matthias Erll <matthias@precisdigital.com>
2016-06-22 16:00:29 +0200

    Updated version and setup.

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ setup(
     name='googleads-requests',
     version=__version__,
     packages=find_packages(),
-    install_requires=['requests', 'googleads'],
+    # suds_requests cannot be added here, as it refers to suds, conflicting with suds-jurko
+    install_requires=['requests', 'googleads>=4.2.0'],
     dependency_links=['git+https://github.com/Precis/googleads-python-lib.git@precis-2#egg=googleads-precis'],
     url='https://github.com/Precis/googleads-requests',
     license='MIT',
