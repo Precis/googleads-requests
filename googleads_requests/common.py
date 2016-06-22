cb1552c43bf1b914bb98bd534522c4befdc732a2
Matthias Erll <matthias@precisdigital.com>
2016-06-22 16:07:38 +0200

    Not subclassing ProxyConfig any longer. Added docstrings.

--- googleads_requests/common.py
+++ googleads_requests/common.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-from googleads.common import ProxyConfig
 from requests import Session
 from suds_requests import RequestsTransport
 
 
-class RequestsProxyConfig(ProxyConfig):
+class RequestsProxyConfig(object):
     """
-    A utility for configuring the usage of requests.
+    A utility for configuring the usage of requests. Provides the same interface as
+    :class:`googleads.common.ProxyConfig`.
     """
     def __init__(self, http_proxy=None, https_proxy=None, cafile=None,
                  disable_certificate_validation=False):
@@ -23,11 +23,10 @@ class RequestsProxyConfig(ProxyConfig):
 
     def GetHandlers(self):
         """
-        Dummy.
+        Dummy. The client prepares an urllib2 call, but we will not use it.
 
-        Returns:
-          A list of urllib2.BaseHandler subclasses to be used when making calls
-          with proxy.
+        :return: An empty list.
+        :rtype: list
         """
         return []
 
@@ -35,12 +34,17 @@ class RequestsProxyConfig(ProxyConfig):
         """
         Retrieve a suds.transport.http.HttpTransport to be used with suds.
 
-        Returns:
-          A RequestsTransport instance used to make requests with suds using the
-          requests transport.
+        :return: A RequestsTransport instance used to make requests with suds using the requests transport.
+        :rtype: suds_requests.RequestsTransport
         """
         return RequestsTransport(self._session)
 
     @property
     def session(self):
+        """
+        Returns the session object.
+
+        :return: Requests session to be used in suds.
+        :rtype: requests.Session
+        """
         return self._session
